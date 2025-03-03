02e2bb5118f203a4f3a86e6e8a0559b7049fe152
sylksatin <timesomethingok@gmail.com>
2025-03-03 17:14:58 +0000

    new

--- GeneralUI.java
+++ GeneralUI.java
@@ -49,16 +49,18 @@ public class GeneralUI extends Application {
 
         // center: generic placeholder content, add London later
         Label contentLabel = new Label("Main Content Area");
-        contentLabel.getStyleClass().add("content-area");;
+        contentLabel.getStyleClass().add("content-area");
         root.setCenter(contentLabel);
 
-        // bottom: co-ordinated
-        Label footer = new Label("Co-ordinates");
+        // bottom: co-ordinates
+        Label footer = new Label("Co-ordinates:");
         footer.getStyleClass().add("coordinates");
         root.setBottom(footer);
 
         // create the primary scene (the map view)
         mainScene = new Scene(root, 800, 500);
+        
+        //connect external css file
         mainScene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         
         //create the secondary scene (the stats view)
@@ -75,26 +77,50 @@ public class GeneralUI extends Application {
 
     // creates secondary scene for stats section
     private Scene createSecondaryScene(String title, Stage primaryStage) {
+        // creating the borderpane
+        BorderPane root = new BorderPane();
+        root.getStyleClass().add("secondary-background");
+
+        // make the header, containing the map and stats buttons horizontally
+        HBox header = new HBox();
+        header.getStyleClass().add("header");
+
+        Button mapButton = new Button("Map");
+        Button statsButton = new Button("Stats");
+
+        mapButton.getStyleClass().add("header-button");
+        statsButton.getStyleClass().add("header-button");
+
+        // list of UI features the header should have
+        header.getChildren().addAll(mapButton, statsButton);
+        root.setTop(header);
+
+        // left sidebar with blahblah
+        VBox sideBar = new VBox(15);
+        sideBar.getStyleClass().add("sidebar");
+        sideBar.setPrefWidth(200);
+        sideBar.setAlignment(Pos.TOP_CENTER);
         
-        //creates vertical space for placeholder labels and buttons in page
-        VBox layout = new VBox(20);
-        layout.setAlignment(Pos.CENTER);
-        layout.getStyleClass().add("secondary-page");
-        
-        //create stats for secondary page
+        // bottom: co-ordinates
+        Label footer = new Label("Co-ordinates:");
+        footer.getStyleClass().add("coordinates");
+        root.setBottom(footer);
+
+        // create stats title for secondary page
         Label label = new Label(title);
-        label.getStyleClass().add("stats-title");
+        label.getStyleClass().add("content-area");
+
+        // functionality of map button to go back to primary scene
+        mapButton.setOnAction(e -> primaryStage.setScene(mainScene));
+
+        // set center content
+        root.setCenter(label);
+        root.setLeft(sideBar);
+
+        // create and return the new scene
+        Scene scene = new Scene(root, 800, 500);
         
-        //creation of back button, to go back to primary scene
-        Button backButton = new Button("Back");
-        
-        //functionality of back button to go back to primary scene
-        backButton.setOnAction(e -> primaryStage.setScene(mainScene));
-        
-        //list of UI features in layout
-        layout.getChildren().addAll(label, backButton);
-        
-        Scene scene = new Scene(layout, 800, 500);
+        // connect external css file
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         return scene;
     }
