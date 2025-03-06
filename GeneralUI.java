787207614a523f899563e45a37a5fd5e17d34f71
sylksatin <timesomethingok@gmail.com>
2025-03-06 16:25:53 +0000

    j

--- GeneralUI.java
+++ GeneralUI.java
@@ -44,6 +44,12 @@ public class GeneralUI extends Application {
         ComboBox<String> dropdown2 = new ComboBox<>();
         dropdown2.getItems().addAll("NO2", "PM10", "PM2.5");
         
+        
+        
+        //Listeners for the comboboxes
+        dropdown1.setOnAction(event -> handleComboBoxSelection(dropdown1, dropdown2));
+        dropdown2.setOnAction(event -> handleComboBoxSelection(dropdown1, dropdown2));
+
         // add the dropdown boxes to the sidebar 
         mapSideBar.getChildren().addAll(dropdown1Label, dropdown1, dropdown2Label, dropdown2);
         mapLayout.setLeft(mapSideBar);
@@ -112,6 +118,31 @@ public class GeneralUI extends Application {
         // display primaryStage
         primaryStage.setScene(mainScene);
         primaryStage.show();
+    }
+    
+    // Event handler for combo boxes
+    public void handleComboBoxSelection(ComboBox<String> dropdown1, ComboBox<String> dropdown2) {
+        String year = dropdown1.getSelectionModel().getSelectedItem();
+        String pollutant = dropdown2.getSelectionModel().getSelectedItem();
+        if (year != null && pollutant != null) {
+            String filename = "";
+            switch (pollutant) {
+            case "NO2":
+                filename = String.format("UKAirPollutionData/%s/mapno2%s.csv", pollutant, year);
+                break;
+            case "PM2.5":
+                filename = String.format("UKAirPollutionData/%s/mappm25%sg.csv", pollutant, year);
+                break;
+            case "PM10":
+                filename = String.format("UKAirPollutionData/%s/mappm10%sg.csv", pollutant, year);
+                break;
+            default:
+                System.out.println("Unknown file loaded");
+            }
+            if (!filename.equals("")){
+                new FileLoadDemo(filename);
+            }
+        }
     }
     
     //used to launch the program
