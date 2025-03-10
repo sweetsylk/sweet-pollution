faf01e654d88737325dc4148da9543fa7aad7eb2
sylksatin <timesomethingok@gmail.com>
2025-03-10 15:33:56 +0000

    k

--- WelcomeScreen.java
+++ WelcomeScreen.java
@@ -31,7 +31,10 @@ public class WelcomeScreen extends Application {
 
         // when clicked, switch to the main page (GeneralUI)
         continueButton.setOnAction(e -> {
-            GeneralUI mainApp = new GeneralUI(); //instantiate the class
+
+
+            GeneralUI mainApp = new GeneralUI(); //instantiate the general UI class
+
             mainApp.start(primaryStage); // switch scene to main page   
         });
 
