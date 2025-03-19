3c7b17f03a3e179a6164829e78a4025c97f14282
sylksatin <timesomethingok@gmail.com>
2025-03-19 19:10:11 +0000

    hid API and fixed the recency of API data and slowed down requests to meet the rate limits

--- DataHandler.java
+++ DataHandler.java
@@ -1,112 +1,111 @@
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 public class DataHandler {
+    public DataHandler() {
+    }
 
-    // Gets the average pollutant trend over years
     public static ArrayList<Double> getPollutantTrends(String pollutant) {
-        List<Double> trend = new ArrayList<>();
+        List<Double> trend = new ArrayList();
 
-        for (int year_index = 2018; year_index <= 2023; year_index++) {
+        for(int year_index = 2018; year_index <= 2023; ++year_index) {
             String year = String.valueOf(year_index);
             String filename = generateFilename(pollutant, year);
-            if (filename.isEmpty()) {
-                continue;
+            if (!filename.isEmpty()) {
+                DataLoader loader = new DataLoader();
+                DataSet dataSet = loader.loadDataFile(filename);
+                trend.add(getAveragePollutantLevel(dataSet));
             }
-
-            DataLoader loader = new DataLoader();
-            DataSet dataSet = loader.loadDataFile(filename);
-            trend.add(getAveragePollutantLevel(dataSet));
         }
 
-        return (ArrayList<Double>) trend;
+        return (ArrayList)trend;
     }
 
-    // Gets the highest pollutant trend over years
     public static ArrayList<Double> getHighestPollutantTrends(String pollutant) {
-        List<Double> trend = new ArrayList<>();
+        List<Double> trend = new ArrayList();
 
-        for (int year_index = 2018; year_index <= 2023; year_index++) {
+        for(int year_index = 2018; year_index <= 2023; ++year_index) {
             String year = String.valueOf(year_index);
             String filename = generateFilename(pollutant, year);
-            if (filename.isEmpty()) {
-                continue;
-            }
-
-            DataLoader loader = new DataLoader();
-            DataSet dataSet = loader.loadDataFile(filename);
-            ArrayList<Double> highestPollutantData = getHighestPollutantLevel(dataSet);
-
-            if (!highestPollutantData.isEmpty()) {
-                trend.add(highestPollutantData.get(0)); // Store the highest value
+            if (!filename.isEmpty()) {
+                DataLoader loader = new DataLoader();
+                DataSet dataSet = loader.loadDataFile(filename);
+                ArrayList<Double> highestPollutantData = getHighestPollutantLevel(dataSet);
+                if (!highestPollutantData.isEmpty()) {
+                    trend.add((Double)highestPollutantData.get(0));
+                }
             }
         }
 
-        return (ArrayList<Double>) trend;
+        return (ArrayList)trend;
     }
 
-    // Generates the correct filename for a given pollutant and year
     public static String generateFilename(String pollutant, String year) {
-        return switch (pollutant) {
-            case "NO2" -> String.format("UKAirPollutionData/%s/mapno2%s.csv", pollutant, year);
-            case "PM2.5" -> String.format("UKAirPollutionData/%s/mappm25%sg.csv", pollutant, year);
-            case "PM10" -> String.format("UKAirPollutionData/%s/mappm10%sg.csv", pollutant, year);
-            default -> {
+        String var10000;
+        switch (pollutant) {
+            case "NO2":
+                var10000 = String.format("UKAirPollutionData/%s/mapno2%s.csv", pollutant, year);
+                break;
+            case "PM2.5":
+                var10000 = String.format("UKAirPollutionData/%s/mappm25%sg.csv", pollutant, year);
+                break;
+            case "PM10":
+                var10000 = String.format("UKAirPollutionData/%s/mappm10%sg.csv", pollutant, year);
+                break;
+            default:
                 System.out.println("Error occurred");
-                yield "";
-            }
-        };
+                var10000 = "";
+        }
+
+        return var10000;
     }
 
-    // Calculates the average pollutant level from dataset
     public static double getAveragePollutantLevel(DataSet data) {
         double total = 0.0;
 
-        for (DataPoint point : data.getData()) {
-            total += point.value();
+        DataPoint point;
+        for(Iterator var3 = data.getData().iterator(); var3.hasNext(); total += point.value()) {
+            point = (DataPoint)var3.next();
         }
 
-        return data.getData().isEmpty() ? 0 : total / data.getData().size();
+        return data.getData().isEmpty() ? 0.0 : total / (double)data.getData().size();
     }
 
-    /**
-     * Get the ten highest pollutant values
-     * @return A list of the 10 highest pollutant values
-     */
     public static ArrayList<Double> getHighestPollutantLevel(DataSet dataset) {
-        ArrayList<Double> highestPLs = new ArrayList<>();
-
+        ArrayList<Double> highestPLs = new ArrayList();
         List<DataPoint> data = dataset.getData();
         data.sort(Comparator.comparingDouble(DataPoint::value).reversed());
-
         int maxElements = Math.min(10, data.size());
-        for (int i = 0; i < maxElements; i++) {
-            highestPLs.add(data.get(i).value());
+
+        for(int i = 0; i < maxElements; ++i) {
+            highestPLs.add(((DataPoint)data.get(i)).value());
         }
 
         return highestPLs;
     }
 
-    // Gets the average pollutant level for a specific area over years
     public static double getAveragePollutantLevelForArea(String pollutant, int UGC) {
         double total = 0.0;
         int numberOfYears = 0;
 
-        for (int year = 2018; year <= 2023; year++) {
+        for(int year = 2018; year <= 2023; ++year) {
             String filename = generateFilename(pollutant, String.valueOf(year));
             DataLoader loader = new DataLoader();
             DataSet dataSet = loader.loadDataFile(filename);
             List<DataPoint> data = dataSet.getData();
-
-            Optional<DataPoint> result = data.stream().filter(dataPoint -> dataPoint.gridCode() == UGC).findFirst();
+            Optional<DataPoint> result = data.stream().filter((dataPoint) -> {
+                return dataPoint.gridCode() == UGC;
+            }).findFirst();
             if (result.isPresent()) {
-                total += result.get().value();
-                numberOfYears++;
+                total += ((DataPoint)result.get()).value();
+                ++numberOfYears;
             }
         }
 
-        return numberOfYears == 0 ? 0.0 : total / numberOfYears;
+        return numberOfYears == 0 ? 0.0 : total / (double)numberOfYears;
     }
 }
---
+++ LocationData.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class LocationData {
+    private int locationId;
+    private String locationName;
+    private double latitude;
+    private double longitude;
+    private Map<Integer, String> sensorIdToParameter;
+
+    public LocationData(JSONObject locationJson) {
+        this.locationId = locationJson.getInt("id");
+        this.locationName = locationJson.getString("name");
+        JSONObject coords = locationJson.getJSONObject("coordinates");
+        this.latitude = coords.getDouble("latitude");
+        this.longitude = coords.getDouble("longitude");
+        this.sensorIdToParameter = new HashMap();
+        JSONArray sensors = locationJson.getJSONArray("sensors");
+
+        for(int i = 0; i < sensors.length(); ++i) {
+            JSONObject sensor = sensors.getJSONObject(i);
+            int sensorId = sensor.getInt("id");
+            String parameterName = sensor.getJSONObject("parameter").getString("name");
+            this.sensorIdToParameter.put(sensorId, parameterName);
+        }
+
+    }
+
+    public int getLocationId() {
+        return this.locationId;
+    }
+
+    public String getLocationName() {
+        return this.locationName;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public Map<Integer, String> getSensorIdToParameter() {
+        return this.sensorIdToParameter;
+    }
+}
